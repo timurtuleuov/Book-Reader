4daad2601d9af2a7a9839c2cae8fd3446266fe18
Timur Tuleuov <timmmur123@gmail.com>
2023-11-26 00:58:39 +0600

    feat: Count pages

--- app/src/main/java/space/tuleuov/bookreader/ui/reader/readerview/readerUI.kt
+++ app/src/main/java/space/tuleuov/bookreader/ui/reader/readerview/readerUI.kt
@@ -6,11 +6,8 @@ import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
@@ -21,6 +18,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.Hyphens
@@ -29,26 +29,40 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.Json.Default.configuration
+import space.tuleuov.bookreader.db.entity.Book
 import space.tuleuov.bookreader.hyphe.Haaivin
 
 import space.tuleuov.bookreader.ui.reader.fb2reader.FB2Book
-
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun readerUI(book: FB2Book, navController: NavController, haaivin: Haaivin) {
-    // Состояние видимости баров
     val barStates = rememberSaveable { mutableStateOf(false) }
     val interactionSource = remember { MutableInteractionSource() }
-    // Функция для скрытия баров
-    fun hideBars() {
-        barStates.value = false
-    }
 
-    // Функция для показа баров
-    fun showBars() {
-        barStates.value = true
+    var columnHeightPx by remember {
+        mutableStateOf(0)
+    }
+    val heightPage = 1427
+//    val pageCountLimit by remember{
+//        mutableStateOf(0)
+//    }
+    var pageCount by remember {
+        mutableStateOf(0)
+    }
+    LaunchedEffect(book) {
+        val result = withContext(Dispatchers.IO) {
+            countPageAsync(book)
+        }
+
+        pageCount = result
     }
 
     com.google.accompanist.insets.ui.Scaffold(
@@ -56,11 +70,18 @@ fun readerUI(book: FB2Book, navController: NavController, haaivin: Haaivin) {
             TopBar(navController = navController, title = book.title, barStates = barStates)
         },
         bottomBar = {
-            BottomBar(barStates = barStates)
+            BottomBar(barStates = barStates, pageCount)
         }
     ) {
-        LazyColumn(modifier = Modifier
-            .clickable( onClick = { toggleBars(barStates) }, indication = null, interactionSource = interactionSource)
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable(
+                    onClick = { toggleBars(barStates) },
+                    indication = null,
+                    interactionSource = interactionSource
+                )
+
         ) {
             items(book.chapters.drop(1)) { chapter ->
                 Text(
@@ -70,33 +91,35 @@ fun readerUI(book: FB2Book, navController: NavController, haaivin: Haaivin) {
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(bottom = 20.dp, top = 20.dp)
+
                 )
                 chapter.content.lines().forEach { line ->
                     val hyphLine = haaivin.hyphenate(string = line, dictionaryId = "ruhyph")
                     Text(
-
                         text = "      " + hyphLine,
-                        modifier = Modifier.padding(horizontal = 8.dp),
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp)
+                            ,
                         textAlign = TextAlign.Justify,
                         style = TextStyle(
                             fontSize = 18.sp,
                             hyphens = Hyphens.Auto,
                             lineBreak = LineBreak.Paragraph,
-
-                            ),
-
                         )
-
+                    )
                 }
             }
         }
     }
+
+
 }
+
 private fun toggleBars(barStates: MutableState<Boolean>) {
     barStates.value = !barStates.value
 }
 @Composable
-fun BottomBar(barStates: MutableState<Boolean>) {
+fun BottomBar(barStates: MutableState<Boolean>, pageCount: Int) {
     AnimatedVisibility(
         visible = barStates.value,
         enter = slideInVertically(initialOffsetY = { it }),
@@ -105,7 +128,7 @@ fun BottomBar(barStates: MutableState<Boolean>) {
             BottomAppBar(){
                 var sliderPosition by remember { mutableFloatStateOf(0f) }
                 Column(modifier = Modifier.fillMaxWidth()) {
-                    Text(text = "1 из 100", modifier = Modifier.align(Alignment.CenterHorizontally))
+                    Text(text = "1 из $pageCount", modifier = Modifier.align(Alignment.CenterHorizontally))
                     Slider(
                         colors = SliderDefaults.colors(
                             thumbColor = Color.White,
@@ -140,4 +163,20 @@ fun TopBar(navController: NavController, title: String, barStates: MutableState<
             )
         }
     )
+}
+
+suspend fun countPageAsync(book: FB2Book): Int {
+    delay(1000)
+    var pageCount = 0
+    var columnHeightPx = 0
+    book.chapters.forEach { chapter ->
+        // Прибавляем высоту каждой строки текста к общей высоте
+        columnHeightPx += 114
+        chapter.content.lines().forEach {line ->
+            val countLine = line.length / 40
+            columnHeightPx += countLine * 38
+        }
+    }
+    pageCount = columnHeightPx / 1427
+    return pageCount
 }
